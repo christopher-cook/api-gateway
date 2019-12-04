9e0a13ebb09a711ef7665ddb7b63a23d269fff1d
Christopher Cook <cgcook81@gmail.com>
2019-12-04 09:08:52 -0500

    fixed test - passed

--- src/test/java/com/example/apigateway/repository/UserRepositoryTest.java
+++ src/test/java/com/example/apigateway/repository/UserRepositoryTest.java
@@ -5,49 +5,56 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.context.junit4.SpringRunner;
 
 //import static org.junit.Assert.assertEquals;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 //import static org.mockito.Mockito.when;
 
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
 public class UserRepositoryTest {
 
     @InjectMocks
     UserRepository userRepository;
 
-    @Mock
-    UserBean user;
 
     @Mock
     ResultSet rs;
+
     @Mock
     JdbcTemplate jdbc;
 
+    @Captor
+    private ArgumentCaptor<RowMapper<UserBean>> rowMapperCaptor;
+
+
     @Before
-    public void init() {
-        user.setId(1L);
-        user.setUsername("chris");
-        user.setPassword("testPass");
-        user.setEmail("test@email.com");
+    public void init() throws SQLException {
+       UserBean user = new UserBean(1L, "cgcook81@gmail.com", "chris", "testPass");
+
+        when(rs.getLong(any())).thenReturn(user.getId());
+        when(rs.getString(any())).thenReturn(user.getEmail(), user.getUsername(), user.getPassword());
     }
 
     @Test
-    public void getUserBean_Success() {
-        ArgumentCaptor <UserBean> rowSet = ArgumentCaptor.forClass(UserBean.class);
-        UserBean user = userRepository.getUserByUsername("chris");
-        verify(jdbc).queryForObject(anyString(), (RowMapper<Object>) any(), rowSet.capture());
+    public void getUserBean_Success() throws SQLException {
+        userRepository.getUserByUsername("chris");
+        verify(jdbc).queryForObject(anyString(), any(), rowMapperCaptor.capture());
 
+        RowMapper<UserBean> rm = rowMapperCaptor.getValue();
+        UserBean returnedUser = rm.mapRow(rs, 1);
 
     }
 }
